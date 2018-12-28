ae5c62ffc464fd8293f5afc1ffbdc5a831b80dd9
RBturtle <35867962+RBturtle@users.noreply.github.com>
2018-12-28 10:02:00 +0100

    Add files via upload

---
+++ Git_Practice_Script.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace AssemblyCSharp
+{
+    public class Git_Practice_Script
+    {
+		static void Main()
+		{
+			Console.WriteLine("Hello World! Evil Brother!!");
+			
+		}
+    }
+}
